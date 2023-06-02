855e75c883066f97fd01826f6c5b153fd9296e6d
sowmik-sec <sowmik.sec@gmail.com>
2023-06-02 09:27:04 +0600

    create input field and add style

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import "./styles.css";
 
-const InputField = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo }) => {
   return (
     <form className="input">
-      <input type="input" placeholder="Enter a task" className="inputBox" />
+      <input
+        type="input"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        placeholder="Enter a task"
+        className="inputBox"
+      />
       <button className="inputSubmit" type="submit">
         Go
       </button>
